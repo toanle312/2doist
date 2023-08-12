a9316761730b7c6b062da5cb525fc5027617bf9d
20120386 <tuanlam689@gmail.com>
2023-08-12 11:01:07 +0700

    (refactor): setup folder and config assets for project

---
+++ src/assets/index.tsx
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import today from "./today.svg";
+import upcoming from "./upcoming.svg";
+import filter from "./filter.svg";
+import inbox from "./inbox.svg";
+import homeBtn from "./homeBtn.svg";
+import helpBtn from "./helpBtn.svg";
+import notiBtn from "./notiBtn.svg";
+import productivityBtn from "./productivityBtn.svg";
+import addTaskBtn from "./addTaskBtn.svg";
+
+export {
+  logo,
+  today,
+  upcoming,
+  filter,
+  inbox,
+  homeBtn,
+  helpBtn,
+  notiBtn,
+  productivityBtn, 
+  addTaskBtn
+};
+
+export const home =
+  "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f3e1.svg";
+export const myWork =
+  "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f3af.svg";
+export const education =
+  "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f4da.svg";
+export const routines =
+  "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f501.svg";
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      color:{
+        primary: "#db4c3f",
+        
+      }
+    },
+  },
+  plugins: [],
+};
